b752b43f80ca8c3263a487b5ecc640a10e07ab84
Andrew <andrew.freelance@i.ua>
2016-03-25 17:56:29 +0200

    some fixes in doc strings

--- btctradeapi/decorators.py
+++ btctradeapi/decorators.py
@@ -1,9 +1,11 @@
+from functools import wraps
+
 from exceptions import UnknownDeal
 from deals import DEALS
 
 
 def dealsfilter(func):
-
+    @wraps(func)
     def wrapper(classinstance, deal, *args, **kwargs):
         if deal not in DEALS.getitems():
             raise UnknownDeal
